13da3c7c1c3e87a6c98fb8f342e77696ebf15468
goldendk <goldendk@hotmail.com>
2023-03-12 16:48:52 +0100

    spring boot resource server works with keycloak now.

--- src/resourceserver/src/main/java/org/example/resourceserver/config/ResourceServerConfig.java
+++ src/resourceserver/src/main/java/org/example/resourceserver/config/ResourceServerConfig.java
@@ -1,26 +1,33 @@
 package org.example.resourceserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class ResourceServerConfig {
 
+    public ResourceServerConfig(@Value("${foo.bar}") String port) {
+        System.out.println("#");
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.mvcMatcher("/articles/**")
-          .authorizeRequests()
-          .mvcMatchers("/articles/**")
-          .access("hasAuthority('SCOPE_articles.read')")
-          .and()
-          .oauth2ResourceServer(oauth2->oauth2.jwt());
+                .authorizeRequests()
+
+
+                .mvcMatchers("/articles/**")
+                //.access("hasAuthority('SCOPE_articles.read')")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
         return http.build();
     }
 
+    private final String issuerUri = "http://localhost:8080/realms/dev";
 
 
 }
